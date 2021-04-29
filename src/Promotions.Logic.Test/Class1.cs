a41f0d1de7c8d21e9b802872dd9a09a5a50f1a42
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-04-29 16:45:48 +0200

    Add initial project structure

---
+++ src/Promotions.Logic.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Promotions.Logic.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Promotions.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Promotions.Logic
+{
+    public class Class1
+    {
+    }
+}
