c92e0face8719e0a6ded196d947cc11921978b64
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-04-29 18:32:42 +0200

    Add second rule

---
+++ src/Promotions.Logic.Test/TestTwoOfB.cs
@@ -0,0 +1,63 @@
+﻿using AutoFixture;
+using Moq;
+using Promotions.Entity.Models;
+using Promotions.Logic.Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Promotions.Logic.Test
+{
+    public class TestTwoOfB
+    {
+        [Fact]
+        public void TestOnlyOne()
+        {
+            // arrange
+            var products = Utility.CreateProducts(1, "B", 30, false);
+            var order = Utility.CreateOrder(products, 0);
+            var threeOfA = new PromotionTwoOfB();
+
+            // act
+            threeOfA.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 0);
+        }
+
+        [Fact]
+        public void TestMoreThanThree()
+        {
+            // arrange
+            var products = Utility.CreateProducts(10, "B", 30, false);
+            var order = Utility.CreateOrder(products, 0);
+            var threeOfA = new PromotionTwoOfB();
+
+            // act
+            threeOfA.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 15);
+            Assert.True(order.Products.Take(2).All(p => p.HasBeenDiscounted == true));
+        }
+
+        [Fact]
+        public void TestExactlyThree()
+        {
+            // arrange
+            var products = Utility.CreateProducts(2, "B", 30, false);
+            var order = Utility.CreateOrder(products, 0);
+            var threeOfA = new PromotionTwoOfB();
+
+            // act
+            threeOfA.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 15);
+            Assert.True(order.Products.All(p => p.HasBeenDiscounted == true));
+        }
+    }
+}
---
+++ src/Promotions.Logic/Promotions/PromotionTwoOfB.cs
@@ -0,0 +1,29 @@
+﻿using Promotions.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotions.Logic.Promotions
+{
+    public class PromotionTwoOfB : IPromotion
+    {
+        const int PromotionValue = 15;
+
+        public void Apply(Order order)
+        {
+            var toDiscount = order.Products
+                .Where(p => !p.HasBeenDiscounted)
+                .Where(p => p.Sku == "B")
+                .Take(2)
+                .ToList();
+
+            if (toDiscount.Count == 2)
+            {
+                order.TotalDiscount += PromotionValue;
+                toDiscount.ForEach(p => p.HasBeenDiscounted = true);
+            }
+        }
+    }
+}
