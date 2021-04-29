7123ff6c4a3433d535bdbb2001367cc0597cf00a
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-04-29 18:42:39 +0200

    Add last rule

---
+++ src/Promotions.Logic.Test/TestOneOfCAndD.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using Moq;
+using Promotions.Entity.Models;
+using Promotions.Logic.Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Promotions.Logic.Test
+{
+    public class TestOneOfCAndD
+    {
+        [Fact]
+        public void TestNotEnough()
+        {
+            // arrange
+            var products = Utility.CreateProducts(1, "C", 20, false);
+            var order = Utility.CreateOrder(products, 0);
+            var oneOfCAndD = new PromotionOneOfCAndD();
+
+            // act
+            oneOfCAndD.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 0);
+        }
+
+        [Fact]
+        public void TestMoreThanEnough()
+        {
+            // arrange
+            var cs = Utility.CreateProducts(5, "C", 20, false);
+            var ds = Utility.CreateProducts(4, "D", 15, false);
+            var order = Utility.CreateOrder(cs.Union(ds), 0);
+            var oneOfCAndD = new PromotionOneOfCAndD();
+
+            // act
+            oneOfCAndD.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 5);
+            Assert.Equal(2, order.Products.Where(p => p.HasBeenDiscounted == true).Count());
+        }
+
+        [Fact]
+        public void TestExactlyEnough()
+        {
+            // arrange
+            var cs = Utility.CreateProducts(1, "C", 20, false);
+            var ds = Utility.CreateProducts(1, "D", 15, false);
+            var order = Utility.CreateOrder(cs.Union(ds), 0);
+            var oneOfCAndD = new PromotionOneOfCAndD();
+
+            // act
+            oneOfCAndD.Apply(order);
+
+            // assert
+            Assert.True(order.TotalDiscount == 5);
+            Assert.True(order.Products.All(p => p.HasBeenDiscounted == true));
+        }
+    }
+}
---
+++ src/Promotions.Logic/Promotions/PromotionOneOfCAndD.cs
@@ -0,0 +1,28 @@
+﻿using Promotions.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotions.Logic.Promotions
+{
+    public class PromotionOneOfCAndD : IPromotion
+    {
+        const int PromotionValue = 5;
+
+        public void Apply(Order order)
+        {
+            var c = order.Products
+                .FirstOrDefault(p => !p.HasBeenDiscounted && p.Sku == "C");
+            var d = order.Products
+                .FirstOrDefault(p => !p.HasBeenDiscounted && p.Sku == "D");
+
+            if (c is not null && d is not null)
+            {
+                order.TotalDiscount += PromotionValue;
+                new List<Product> { c, d }.ForEach(p => p.HasBeenDiscounted = true);
+            }
+        }
+    }
+}
